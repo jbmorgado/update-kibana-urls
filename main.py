73e925c6d67cbab5b39c59e12153d07e6f31fc3c
Jorge Bruno Morgado <jb.morgado@gmail.com>
2021-12-23 15:36:24 +0000

    added argument parssing options

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
+import argparse
 import requests
 import json
 from colorama import Fore
 
-# KIBANA_IP = '10.11.115.6'
-KIBANA_IP = "10.11.115.13"
-OLD_IP = "13.80.150.4"
 WARNS = 0
 
-# TODO: Explore possibility of altering scripts as well. These probably must be done outside of the Index Pattern logic. See: https://www.elastic.co/guide/en/elasticsearch/reference/current/create-stored-script-api.html
 
-
-def get_index_paterns():
-    # ploads = {'things':2,'total':25}
-    # r = requests.get('https://httpbin.org/get',params=ploads)
-    # TODO: add payload
-    # TODO: handle `per_page`
+def get_index_paterns(ip):
+    """
+    Retrieves all the Index Patterns present at the elasticsearch server with ip {ip}.
+    :param ip: the ip of the elasticsearch server
+    :return: list with Index Patterns ID identifier
+    """
+    ids = []
     try:
         response = requests.get(
-            f"http://{KIBANA_IP}:5601/api/saved_objects/_find?type=index-pattern&search_fields=title&search=*&per_page=1000"
+            f"http://{ip}:5601/api/saved_objects/_find?type=index-pattern&search_fields=title&search=*&per_page=1000"
         )
         json_response = response.json()
         print("\tINDEX PATTERNS")
         print("\t--------------")
         print("\tID\t\t\t\t\tTITLE")
-        ids = []
         for obj in json_response["saved_objects"]:
             p_id = obj["id"]
             p_title = obj["attributes"]["title"]
@@ -34,27 +31,28 @@ def get_index_paterns():
         print("\tUnable to get index patterns from server.")
 
 
-def replace_pattern_fields(pattern_id):
-    # ploads = {'things':2,'total':25}
-    # r = requests.get('https://httpbin.org/get',params=ploads)
-    # TODO: add payload
-    # TODO: handle `per_page`
-    response = requests.get(
-        f"http://{KIBANA_IP}:5601/api/index_patterns/index_pattern/{pattern_id}"
-    )
+def replace_fields_ip(id, ip, old_ip):
+    """
+    Iterates over all fields in an Index Pattern and replaces any occurence of {old_ip} for {ip}.
+    This method doesn't work on script fields and at the present those must be altered manualy.
+    TODO: Explore possibility of altering scripts as well. These probably must be done outside of the Index Pattern logic.
+    See: https://www.elastic.co/guide/en/elasticsearch/reference/current/create-stored-script-api.html
+    :param id: the id of the Index Pattern
+    :param ip: the proper ip address of the elasticsearch server
+    :param old_ip: the outdated ip that we will be replacing
+    """
+    response = requests.get(f"http://{ip}:5601/api/index_patterns/index_pattern/{id}")
     json_response = response.json()
     title = json_response["index_pattern"]["title"]
-    print(f"\tParsing index pattern: {title} ({pattern_id})...")
-    url = (
-        f"http://{KIBANA_IP}:5601/api/index_patterns/index_pattern/{pattern_id}/fields"
-    )
+    print(f"\tParsing index pattern: {title} ({id})...")
+    url = f"http://{ip}:5601/api/index_patterns/index_pattern/{id}/fields"
     headers = {"kbn-xsrf": "reporting"}
     fields = json_response["index_pattern"]["fields"]
     for field in fields:
         global WARNS
         for item in fields[field]:
             item_str = json.dumps(fields[field][item])
-            if OLD_IP in item_str:
+            if old_ip in item_str:
                 if item == "script":
                     print(
                         Fore.YELLOW
@@ -64,7 +62,7 @@ def replace_pattern_fields(pattern_id):
                     WARNS = WARNS + 1
                 else:
                     print(f"\t\tReplacing URL in field: {field}")
-                    item_str = item_str.replace(OLD_IP, KIBANA_IP)
+                    item_str = item_str.replace(old_ip, ip)
                     data = (
                         f'{{ "fields": {{ "{field}": {{ "{item}": {item_str} }} }} }}'
                     )
@@ -76,17 +74,36 @@ def replace_pattern_fields(pattern_id):
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-ip",
+        type=str,
+        default="10.11.115.13",
+        help="ip address of elasticsearch server",
+        required=False,
+    )
+    parser.add_argument(
+        "-old-ip",
+        type=str,
+        default="13.80.150.4",
+        help="ip address to replace",
+        required=False,
+    )
+    args = parser.parse_args()
+
+    # KIBANA_IP = '10.11.115.6'
+
     print("Gathering Index Patterns present in the database:")
-    ids = get_index_paterns()
+    ids = get_index_paterns(args.ip)
     print("")
     if len(ids) > 0:
         print(
-            f"Found {len(ids)} Index Patterns.\nReplacing {OLD_IP} IP addresses for {KIBANA_IP}:"
+            f"Found {len(ids)} Index Patterns.\nReplacing {args.old_ip} IP addresses for {args.ip}:"
         )
         for id in ids:
-            replace_pattern_fields(id)
+            replace_fields_ip(id, args.ip, args.old_ip)
     else:
-        print("No Index Patterns presen in the database. Exiting.")
+        print("No Index Patterns present in the database. Exiting.")
 
     if WARNS == 0:
         print("Finished succesfull parsing of all Index Patterns.")
